be283a86c3ee242cda1cc9494eb0055d72694350
salimo2002 <89202366+salimo2002@users.noreply.github.com>
2025-04-20 16:08:38 +0300

    refactor code & Create MaterialCardView

---
+++ lib/views/material_card_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MaterialCardView extends StatelessWidget {
+  const MaterialCardView({super.key});
+  static String id = 'MaterialCardView';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
