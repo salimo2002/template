c102197c77700c6587328e572f7d87be3ec03974
salimo2002 <89202366+salimo2002@users.noreply.github.com>
2025-04-20 15:48:58 +0300

    first commit

---
+++ lib/material_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MaterialCard extends StatelessWidget {
+  const MaterialCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
