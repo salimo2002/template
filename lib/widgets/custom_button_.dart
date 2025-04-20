3c7d81ba3218fc8c0fb021cb838b1473c3443d9e
tnttasata <tnttasata@gmail.com>
2025-04-20 16:09:02 +0300

    add CustomButton

--- lib/widgets/custom_button_.dart
+++ lib/widgets/custom_button_.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:template/utils/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key, required this.text, required this.icon});
+final String text;
+final IconData icon;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: kWhite,
+      ),
+      child: Column(children: [Icon(color: kblueAccent, icon),Text(text)]),
+    );
   }
 }
