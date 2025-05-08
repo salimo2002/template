002b5106ca7e69912bb32454232ecf2dc28f3817
taha <asdfghmnt@gmail.com>
2025-05-09 02:58:19 +0300

    edit AddAccountView

--- lib/views/add_account_view.dart
+++ lib/views/add_account_view.dart
@@ -4,6 +4,7 @@ import 'package:template/utils/custom_app_bar.dart';
 import 'package:template/widgets/home%20view%20widgets/custom_container.dart';
 import 'package:template/widgets/home%20view%20widgets/parts_titel.dart';
 import 'package:template/widgets/new%20item%20view%20widgets/container_fields.dart';
+import 'package:template/widgets/new%20item%20view%20widgets/save_and_exite_button.dart';
 import 'package:template/widgets/new%20item%20view%20widgets/text_field_details.dart';
 
 class AddAccountView extends StatelessWidget {
@@ -17,88 +18,103 @@ class AddAccountView extends StatelessWidget {
         title: 'الزبائن و الموردون',
         showIcons: false,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(5),
-          child: Column(
-            children: [
-              ContainerFields(
-                children: [
-                  TextFieldAndDetails(
-                    hintText: 'العملاء و الزبائن',
-                    label: 'الحساب الرئيسي',
-                    controller: TextEditingController(),
-                  ),
-                ],
-              ),
-              ContainerFields(
-                children: [
-                  TextFieldAndDetails(
-                    hintText: 'اسم الحساب',
-                    label: 'اسم الحساب',
-                    controller: TextEditingController(),
-                  ),
-                ],
-              ),
-              ContainerFields(
-                children: [
-                  TextFieldAndDetails(
-                    hintText: 'رقم الهاتف',
-                    label: 'الهاتف',
-                    controller: TextEditingController(),
-                  ),
-                ],
-              ),
-              ContainerFields(
-                children: [
-                  TextFieldAndDetails(
-                    hintText: 'العنوان',
-                    label: 'العنوان',
-                    controller: TextEditingController(),
-                  ),
-                ],
-              ),
-              ContainerFields(
-                children: [
-                  TextFieldAndDetails(
-                    hintText: 'الرقم الضريبي',
-                    label: 'الرقم الضريبي',
-                    controller: TextEditingController(),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              CustomContainer(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
                 child: Column(
                   children: [
-                    PartsTitle(title: 'الرصيد الافتتاحي', color: kBlueAccent),
-                    SizedBox(height: 5),
-                    TextFieldAndDetails(
-                      hintText: '0',
-                      label: 'مدين',
-                      controller: TextEditingController(),
-                      keyType: TextInputType.numberWithOptions(),
+                    ContainerFields(
+                      children: [
+                        TextFieldAndDetails(
+                          hintText: 'العملاء و الزبائن',
+                          label: 'الحساب الرئيسي',
+                          controller: TextEditingController(),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 5),
-                    TextFieldAndDetails(
-                      hintText: '0',
-                      label: 'دائن',
-                      controller: TextEditingController(),
-                      keyType: TextInputType.numberWithOptions(),
+                    ContainerFields(
+                      children: [
+                        TextFieldAndDetails(
+                          hintText: 'اسم الحساب',
+                          label: 'اسم الحساب',
+                          controller: TextEditingController(),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 5),
-                    TextFieldAndDetails(
-                      keyType: TextInputType.numberWithOptions(),
-                      hintText: '0',
-                      label: 'الرصيد الافتتاحي',
-                      controller: TextEditingController(),
+                    ContainerFields(
+                      children: [
+                        TextFieldAndDetails(
+                          hintText: 'رقم الهاتف',
+                          label: 'الهاتف',
+                          controller: TextEditingController(),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 5),
+                    ContainerFields(
+                      children: [
+                        TextFieldAndDetails(
+                          hintText: 'العنوان',
+                          label: 'العنوان',
+                          controller: TextEditingController(),
+                        ),
+                      ],
+                    ),
+                    ContainerFields(
+                      children: [
+                        TextFieldAndDetails(
+                          hintText: 'الرقم الضريبي',
+                          label: 'الرقم الضريبي',
+                          controller: TextEditingController(),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    CustomContainer(
+                      child: Column(
+                        children: [
+                          PartsTitle(
+                            title: 'الرصيد الافتتاحي',
+                            color: kBlueAccent,
+                          ),
+                          SizedBox(height: 5),
+                          TextFieldAndDetails(
+                            hintText: '0',
+                            label: 'مدين',
+                            controller: TextEditingController(),
+                            keyType: TextInputType.numberWithOptions(),
+                          ),
+                          SizedBox(height: 5),
+                          TextFieldAndDetails(
+                            hintText: '0',
+                            label: 'دائن',
+                            controller: TextEditingController(),
+                            keyType: TextInputType.numberWithOptions(),
+                          ),
+                          SizedBox(height: 5),
+                          TextFieldAndDetails(
+                            keyType: TextInputType.numberWithOptions(),
+                            hintText: '0',
+                            label: 'الرصيد الافتتاحي',
+                            controller: TextEditingController(),
+                          ),
+                          SizedBox(height: 5),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: MediaQuery.sizeOf(context).height * 0.326),
+            SaveAndExitButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
     );
