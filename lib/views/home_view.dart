fdf78ce7a7500cce829b7337d2332992098783c8
taha <asdfghmnt@gmail.com>
2025-05-25 01:02:36 +0300

    edo

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,11 +1,7 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:template/cubit/account%20cubit/accounts_cubit.dart';
-import 'package:template/cubit/bill%20cubit/bill_cubit.dart';
-import 'package:template/cubit/category%20cubit/category_cubit.dart';
-import 'package:template/cubit/material%20cubit/material_cubit.dart';
+import 'package:template/Service/bill_service.dart';
 import 'package:template/models/bill_details_model.dart';
 import 'package:template/models/bill_model.dart';
 import 'package:template/utils/constants.dart';
