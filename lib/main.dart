a3365118f2b05f24116d13db15e7f0086e72f2fa
tnttasata <tnttasata@gmail.com>
2025-06-05 00:59:51 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ import 'package:template/views/review_invoices.dart';
 import 'package:template/views/setting_view.dart';
 import 'package:template/views/splash_view.dart';
 import 'package:template/views/detailed_account_statement_views.dart';
+import 'package:template/views/support_details_views.dart';
 import 'package:template/views/support_views.dart';
 import 'package:template/views/tree_accounts_view.dart';
 
@@ -81,6 +82,8 @@ class Template extends StatelessWidget {
           SupportViews.id: (context) => SupportViews(),
           AccountStatementView.id: (context) => AccountStatementView(),
           SupportDetailsViews.id: (context) => SupportDetailsViews(),
+          FundJournalView.id:(context)=>FundJournalView(),
+          DetailedAccountStatementView.id:(context)=>DetailedAccountStatementView()
         },
         initialRoute: SplashVideoScreen.id,
       ),
