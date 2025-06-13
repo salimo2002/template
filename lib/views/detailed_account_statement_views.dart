0c0624067e503b1e8323bf5edb3d34c703883ef7
salimo2002 <89202366+salimo2002@users.noreply.github.com>
2025-06-13 19:32:51 +0300

    fix Scrol error

--- lib/views/detailed_account_statement_views.dart
+++ lib/views/detailed_account_statement_views.dart
@@ -61,89 +61,94 @@ class _DetailedAccountStatementViewState
           padding: const EdgeInsets.all(8.0),
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: BlocBuilder<BillCubit, BillStatus>(
-                builder: (context, state) {
-                  if (state is SuccessStateBill) {
-                    return Table(
-                      defaultColumnWidth: const FixedColumnWidth(100),
-                      border: TableBorder(
-                        verticalInside: BorderSide(
-                          color: Colors.grey.shade400,
-                          width: 1,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: BlocBuilder<BillCubit, BillStatus>(
+                  builder: (context, state) {
+                    if (state is SuccessStateBill) {
+                      return Table(
+                        defaultColumnWidth: const FixedColumnWidth(100),
+                        border: TableBorder(
+                          verticalInside: BorderSide(
+                            color: Colors.grey.shade400,
+                            width: 1,
+                          ),
+                          horizontalInside: BorderSide(
+                            color: Colors.grey.shade400,
+                            width: 1,
+                          ),
                         ),
-                        horizontalInside: BorderSide(
-                          color: Colors.grey.shade400,
-                          width: 1,
-                        ),
-                      ),
-                      children: [
-                        const TableRow(
-                          decoration: BoxDecoration(color: Colors.white),
+                        children: [
+                          const TableRow(
+                            decoration: BoxDecoration(color: Colors.white),
+                            children: [
+                              TableLabels(label: 'الرصيد'),
+                              TableLabels(label: 'التاريخ'),
+                              TableLabels(label: 'البيان'),
+                              TableLabels(label: 'سعر الافرادي'),
+                              TableLabels(label: 'الكمية'),
+                              TableLabels(label: 'الاجمالي'),
+                            ],
+                          ),
+                          ...List.generate(listBillDetails.length, (index) {
+                            final isEven = index % 2 == 0;
+                            final rowColor =
+                                isEven ? Colors.white : Colors.grey.shade200;
+                            return buildDataRow(
+                              statement:
+                                  listBillDetails[index].detSinglePrice
+                                      .toString(),
+                              rowColor: rowColor,
+                              balance:
+                                  listBillDetails[index].detPrice.toString(),
+                              date:
+                                  listBillDetails[index].detQuantity.toString(),
+                              amount:
+                                  '${listBill[index].bilDate!.year.toString()}/${listBill[index].bilDate!.month.toString()}/${listBill[index].bilDate!.day.toString()}',
+                              individualPrice: decodeToUtf8(
+                                listBill[index].bilNote.toString(),
+                              ),
+                              total: '2500',
+                            );
+                          }),
+                        ],
+                      );
+                    } else if (state is LoadingStateBill) {
+                      return Center(child: CircularProgressIndicator());
+                    } else {
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            TableLabels(label: 'الرصيد'),
-                            TableLabels(label: 'التاريخ'),
-                            TableLabels(label: 'البيان'),
-                            TableLabels(label: 'سعر الافرادي'),
-                            TableLabels(label: 'الكمية'),
-                            TableLabels(label: 'الاجمالي'),
+                            Text(
+                              'حدث خطأ حاول جدداً',
+                              style: FontStyleApp.black18.copyWith(
+                                fontSize: getResponsiveText(context, 18),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            IconButton(
+                              onPressed: () {
+                                Navigator.pushNamedAndRemoveUntil(
+                                  context,
+                                  HomeView.id,
+                                  (route) => false,
+                                );
+                              },
+                              icon: Icon(
+                                Icons.refresh,
+                                color: kBlueAccent,
+                                size: 40,
+                              ),
+                            ),
                           ],
                         ),
-                        ...List.generate(listBillDetails.length, (index) {
-                          final isEven = index % 2 == 0;
-                          final rowColor =
-                              isEven ? Colors.white : Colors.grey.shade200;
-                          return buildDataRow(
-                            statement:
-                                listBillDetails[index].detSinglePrice
-                                    .toString(),
-                            rowColor: rowColor,
-                            balance: listBillDetails[index].detPrice.toString(),
-                            date: listBillDetails[index].detQuantity.toString(),
-                            amount:
-                                '${listBill[index].bilDate!.year.toString()}/${listBill[index].bilDate!.month.toString()}/${listBill[index].bilDate!.day.toString()}',
-                            individualPrice: decodeToUtf8(
-                              listBill[index].bilNote.toString(),
-                            ),
-                            total: '2500',
-                          );
-                        }),
-                      ],
-                    );
-                  } else if (state is LoadingStateBill) {
-                    return Center(child: CircularProgressIndicator());
-                  } else {
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'حدث خطأ حاول جدداً',
-                            style: FontStyleApp.black18.copyWith(
-                              fontSize: getResponsiveText(context, 18),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          IconButton(
-                            onPressed: () {
-                              Navigator.pushNamedAndRemoveUntil(
-                                context,
-                                HomeView.id,
-                                (route) => false,
-                              );
-                            },
-                            icon: Icon(
-                              Icons.refresh,
-                              color: kBlueAccent,
-                              size: 40,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                },
+                      );
+                    }
+                  },
+                ),
               ),
             ),
           ),
