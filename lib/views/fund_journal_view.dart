54b20e56c03196d0ff0acef9356db7823063500f
tnttasata <tnttasata@gmail.com>
2025-06-20 22:13:18 +0300

    edit

--- lib/views/fund_journal_view.dart
+++ lib/views/fund_journal_view.dart
@@ -1,4 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:template/cubit/account%20cubit/accounts_cubit.dart';
+import 'package:template/cubit/debit%20cubit/debit_cubit.dart';
+import 'package:template/models/debit_model.dart';
 import 'package:template/utils/custom_app_bar.dart';
 import 'package:template/utils/font_style.dart';
 import 'package:template/utils/responsive_text.dart';
@@ -16,9 +22,32 @@ class FundJournalView extends StatefulWidget {
 class _FundJournalViewState extends State<FundJournalView> {
   TextEditingController firstDateController = TextEditingController();
   TextEditingController lastDateController = TextEditingController();
-
+  late List<DebitModel> debits;
+  List<String> accName = [];
+  List<DebitModel> debitss = [];
   @override
   Widget build(BuildContext context) {
+    debits = context.read<DebitCubit>().debits;
+    for (var element in debits) {
+      if (element.debDate ==
+          DateTime.parse(
+            '${DateTime.now().year}-${DateTime.now().month.toString().padLeft(2, '0')}-18 00:00:00.000',
+          )) {
+        log(element.debDate.toString());
+
+        if (element.accId2 != 0) {
+          context.read<AccountsCubit>().accounts.forEach((elements) {
+            if (elements.accID == element.accId2) {
+              accName.add(elements.accName);
+              debitss.add(element);
+            }
+          });
+        } else {
+          accName.add('لا يوجد حساب');
+        }
+      }
+    }
+
     return Scaffold(
       appBar: customAppBar(
         context: context,
@@ -26,66 +55,89 @@ class _FundJournalViewState extends State<FundJournalView> {
         showIcons: false,
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: SizedBox(
-                    width: MediaQuery.sizeOf(context).width * 0.8,
-                    child: ContainerFields(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              width: MediaQuery.sizeOf(context).width * 0.3,
-                              child: TextFieldDate(
-                                canRead: false,
-                                date: lastDateController,
-                                hoursOrYear: true,
-                                label: 'الى تاريخ',
-                              ),
-                            ),
-                            SizedBox(height: 15),
-                            SizedBox(
-                              width: MediaQuery.sizeOf(context).width * 0.3,
-                              child: TextFieldDate(
-                                canRead: false,
-                                date: firstDateController,
-                                hoursOrYear: true,
-                                label: 'من تاريخ',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: SizedBox(
                   width: MediaQuery.sizeOf(context).width * 0.8,
-
                   child: ContainerFields(
                     children: [
-                      RowInFundJournalView(lable: ':نوع السند', value: 'قبض'),
-                      RowInFundJournalView(lable: ':المبلغ', value: '5000'),
-                      RowInFundJournalView(
-                        lable: ':الحساب المقابل',
-                        value: 'جسومي',
-                      ),
-                      RowInFundJournalView(lable: ':العملة', value: 'دولارو'),
-                      RowInFundJournalView(
-                        lable: ':البيان',
-                        value: 'زوبي اند زوبي',
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.sizeOf(context).width * 0.3,
+                            child: TextFieldDate(
+                              canRead: false,
+                              date: lastDateController,
+                              hoursOrYear: true,
+                              label: 'الى تاريخ',
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          SizedBox(
+                            width: MediaQuery.sizeOf(context).width * 0.3,
+                            child: TextFieldDate(
+                              canRead: false,
+                              date: firstDateController,
+                              hoursOrYear: true,
+                              label: 'من تاريخ',
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ),
-              ],
-            ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: debitss.length,
+                  itemBuilder: (context, index) {
+                    return SizedBox(
+                      width: MediaQuery.sizeOf(context).width * 0.8,
+
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 10,
+                        ),
+                        child: ContainerFields(
+                          children: [
+                            RowInFundJournalView(
+                              lable: ':نوع السند',
+                              value: debitss[index].ty == 0 ? 'دفع' : 'قبض',
+                            ),
+                            RowInFundJournalView(
+                              lable: ':المبلغ',
+                              value: debitss[index].debAmount.toString(),
+                            ),
+                            RowInFundJournalView(
+                              lable: ':الحساب المقابل',
+                              value: accName[index] == '' ? '' : accName[index],
+                            ),
+                            RowInFundJournalView(
+                              lable: ':العملة',
+                              value:
+                                  debitss[index].curId == 0
+                                      ? 'دولار'
+                                      : 'ليرة سورية',
+                            ),
+                            RowInFundJournalView(
+                              lable: ':البيان',
+                              value: debitss[index].debNote,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -107,10 +159,14 @@ class RowInFundJournalView extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Text(
-          value,
-          style: FontStyleApp.black18.copyWith(
-            fontSize: getResponsiveText(context, 15),
+        Flexible(
+          child: Text(
+            value,
+            textAlign: TextAlign.right,
+            maxLines: 2,
+            style: FontStyleApp.black18.copyWith(
+              fontSize: getResponsiveText(context, 15),
+            ),
           ),
         ),
         SizedBox(width: 5),
