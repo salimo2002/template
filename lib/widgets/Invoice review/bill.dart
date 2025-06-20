336d09cbb5cede970ef8ee4a9b1a8588ddeb9a90
taha <asdfghmnt@gmail.com>
2025-06-20 23:16:59 +0300

    edit

--- lib/widgets/Invoice review/bill.dart
+++ lib/widgets/Invoice review/bill.dart
@@ -19,64 +19,90 @@ class Bill extends StatelessWidget {
     required this.note,
     this.billType,
   });
+
   final String paymentStyle;
   final String invoiceNumber;
   final String billDate;
   final String billTime;
-
   final String nameAccuont;
   final String total;
   final String amountPaid;
   final String? billType;
   final String reminingAmount;
-
   final String note;
+
   @override
   Widget build(BuildContext context) {
     return ContainerFields(
       children: [
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              paymentStyle,
-              style: FontStyleApp.blueAccent18.copyWith(
-                fontSize: getResponsiveText(context, 14),
+            Expanded(
+              child: Text(
+                paymentStyle,
+
+                style: FontStyleApp.blueAccent18.copyWith(
+                  fontSize: getResponsiveText(context, 14),
+                ),
               ),
             ),
-            Text(
-              '$invoiceNumber : رقم الفاتورة',
-              style: FontStyleApp.blueAccent18.copyWith(
-                fontSize: getResponsiveText(context, 14),
+            SizedBox(width: 5),
+            Expanded(
+              child: Text(
+                '$invoiceNumber : رقم الفاتورة',
+
+                textAlign: TextAlign.center,
+                style: FontStyleApp.blueAccent18.copyWith(
+                  fontSize: getResponsiveText(context, 14),
+                ),
               ),
             ),
-            Text(
-              billType ?? '',
-              style: FontStyleApp.blueAccent18.copyWith(
-                fontSize: getResponsiveText(context, 14),
+            SizedBox(width: 8),
+            if (billType != null)
+              Expanded(
+                child: Text(
+                  billType!,
+
+                  textAlign: TextAlign.end,
+                  style: FontStyleApp.blueAccent18.copyWith(
+                    fontSize: getResponsiveText(context, 14),
+                  ),
+                ),
               ),
-            ),
           ],
         ),
+        SizedBox(height: 8),
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              billDate,
-              style: FontStyleApp.grey14.copyWith(
-                fontSize: getResponsiveText(context, 14),
+            Expanded(
+              child: Text(
+                billDate,
+
+                style: FontStyleApp.grey14.copyWith(
+                  fontSize: getResponsiveText(context, 14),
+                ),
               ),
             ),
-            Text(
-              billTime,
-              style: FontStyleApp.grey14.copyWith(
-                fontSize: getResponsiveText(context, 14),
+            SizedBox(width: 8),
+            Expanded(
+              child: Text(
+                billTime,
+
+                textAlign: TextAlign.center,
+                style: FontStyleApp.grey14.copyWith(
+                  fontSize: getResponsiveText(context, 14),
+                ),
               ),
             ),
-            Text(
-              nameAccuont,
-              style: FontStyleApp.grey14.copyWith(
-                fontSize: getResponsiveText(context, 14),
+            SizedBox(width: 8),
+            Expanded(
+              child: Text(
+                nameAccuont,
+
+                textAlign: TextAlign.end,
+                style: FontStyleApp.grey14.copyWith(
+                  fontSize: getResponsiveText(context, 14),
+                ),
               ),
             ),
           ],
@@ -90,19 +116,22 @@ class Bill extends StatelessWidget {
           lable3: 'مبلغ الفاتورة',
           value3: total,
         ),
-        note == ''
-            ? SizedBox.shrink()
-            : Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
+        if (note.isNotEmpty)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Text(
                   decodeToUtf8(note),
+                  textAlign: TextAlign.end,
+                  overflow: TextOverflow.visible,
                   style: FontStyleApp.blackCustom18.copyWith(
                     fontSize: getResponsiveText(context, 14),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
       ],
     );
   }
